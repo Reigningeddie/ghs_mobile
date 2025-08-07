69e6565bb5d57093e0f772b87f7e67cda8ea6954
Eduardo Aguilar <eaguilar92105@yahoo.com>
2025-08-06 19:10:30 -0700

    fixed the vids bug, to fit 4 in one row

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -10,7 +10,7 @@ const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 //get specific heights
 const videoBorder = screenHeight / 2.5
-const thirds = screenWidth / 3;
+const thirds = screenWidth / 3 - 0.7;
 
 const router = useRouter();
 
