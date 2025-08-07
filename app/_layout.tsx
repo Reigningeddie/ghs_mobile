20b7069404784bfb64e119d71d7dc80434e4681b
Eduardo Aguilar <eaguilar92105@yahoo.com>
2025-08-06 17:19:46 -0700

    added SafeAreaView to login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,22 @@
-import {Stack} from 'expo-router'
-import {AuthProvider} from '../database/authContext'
-import {StatusBar} from 'expo-status-bar';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {View} from 'react-native';
-
+import { Stack } from 'expo-router';
+import { AuthProvider } from '../database/authContext';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'; // Import SafeAreaProvider
 
 const _layout = () => {
-
   return (
     <AuthProvider>
-      <View style={{height: 50, backgroundColor: 'red'}} />
-      <StatusBar style="auto" />
-      <Stack screenOptions={{headerShown: false,}}>
-        <Stack.Screen
-        name="(auth)"
-        options={{ 
-          headerShown:false,
-        }}
-      />
-              <Stack.Screen
-        name="(main)"
-        options={{ 
-          headerShown:false,
-        }}
-      />
-    </Stack>
+      <SafeAreaProvider> 
+        <SafeAreaView style={{ flex: 1 }}> 
+          <StatusBar style="auto" />
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(main)" options={{ headerShown: false }} />
+          </Stack>
+        </SafeAreaView>
+      </SafeAreaProvider>
     </AuthProvider>
-  )
-}
+  );
+};
 
-export default _layout
+export default _layout;
