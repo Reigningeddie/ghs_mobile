183ced379a2eb859ca637359a8640d4bde9a0189
Eduardo Aguilar <eaguilar92105@yahoo.com>
2025-10-09 14:36:34 -0700

    created the friend request card

--- app/(main)/(tabs)/(_profile)/notifications.tsx
+++ app/(main)/(tabs)/(_profile)/notifications.tsx
@@ -14,10 +14,28 @@ export default function Notifications(): React.JSX.Element {
         <Text style={styles.title}>Notifications</Text>
       </View>
       <ScrollView contentContainerStyle={styles.borderContent}>
-        <View style={styles.horizon}>
-          <Text style={styles.txt}>Friend Requests</Text>
+        <View style={styles.section}>
+          <Text style={styles.titleTxt}>Friend Requests</Text>
+          
+          <View style={styles.requests}>
+              <View style={styles.img} />
+              <View style={styles.info}>
+                <Text style={styles.user}>Credibowl</Text>
+                <Text style={styles.txt}>Sent you a friend request</Text>
+                <Text style={styles.time}>10m</Text>
+              </View>
+              <Pressable style={styles.btn}>
+              <Text style={styles.btnTxt}>Accept</Text>
+            </Pressable>
+          </View>
+
         </View>
-        <Text style={styles.txt}>Grand Hand Slams</Text>
+        <View style={styles.section}>
+          <Text style={styles.titleTxt}>Grand Hand Slams</Text>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.titleTxt}>Likes ♥️</Text>
+        </View>
       </ScrollView>
     </View>
   );
@@ -51,11 +69,51 @@ const styles = StyleSheet.create({
     padding: 20,
     flexGrow: 1,
   },
-  horizon: {
-    
+  section: {
+    flexDirection: 'column',
   },
-  txt: {
+  titleTxt: {
     fontSize: 18,
     fontWeight: 'bold'
   },
+  requests: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  img: {
+    backgroundColor: '#284B63',
+    height: 65,
+    width: 65,
+    borderRadius: 50
+  },
+  info: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '65%',
+  },
+  user: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginRight: 4,
+  },
+  txt: {
+    marginRight: 4,
+  },
+  time: {
+    color: '#636363',
+  },
+  btn: {
+    backgroundColor: '#2EA1DD',
+    borderWidth: 1,
+    borderRadius: 3,
+    borderColor: '#2EA1DD',
+    width: '19%',
+    height: 26,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  btnTxt: {
+    color: 'white',
+  }
 });
